7bd2ee6ad515b9ecf79fd958a8061e36ba8617bf
Melissa Orgill <orgillmelissa33@gmail.com>
2018-02-09 14:54:20 -0800

    tasks can be added via curl

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ tasks = [
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk,Chesse,Pizza,Fruit,Tylenol',
-        'done':False
-     },
+        'done': False
+    },
     {
         'id': 2,
         'title': u'Learn Python',
@@ -18,17 +18,35 @@ tasks = [
 ]
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
-    task = [task for tasks in tasks if task['id'] == task_id]
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': task})
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error':'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
