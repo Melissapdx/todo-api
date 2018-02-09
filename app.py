c1826277349d85ad513ea592cf57a85f8a08513a
Melissa Orgill <orgillmelissa33@gmail.com>
2018-02-09 12:32:58 -0800

    added json 404 error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -10,17 +10,25 @@ tasks = [
         'done':False
      },
     {
-        'id' : 2,
-        'title' : u'Learn Python',
-        'description' : u'Need to find a good Python tutorial on the web',
-        'done' : False
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
     }
 ]
 
 
-@app.route('/')
-def get_tasks():
-    return jsonify({'tasks' : tasks})
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+
+    task = [task for tasks in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
